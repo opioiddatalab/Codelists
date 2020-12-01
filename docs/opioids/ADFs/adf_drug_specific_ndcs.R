0f439bbf8780b962d4a614014d715c2e9f713da8
Bethany <diprete@email.unc.edu>
2020-12-01 12:32:25 -0500

    update ndc format issue

--- docs/opioids/ADFs/adf_drug_specific_ndcs.R
+++ docs/opioids/ADFs/adf_drug_specific_ndcs.R
@@ -20,7 +20,9 @@ path <- str_replace(rstudioapi::getActiveDocumentContext()$path,"adf_drug_specif
 
 all_adfs <- read_csv(str_c(path,"ADF_NDCs.csv")) %>% 
   mutate_if(is.character, str_to_lower) %>% 
-  set_names(tolower(names(.))) 
+  set_names(tolower(names(.))) %>% 
+  mutate(ndc = str_remove_all(ndc, "'"),
+         ndc = str_c("'",ndc,"'")) 
 names(all_adfs) <- tolower(names(all_adfs))
 
 folders <- c("Arymo_ER", "Embeda", "Hysingla_ER", "MorphaBond_ER", 
@@ -34,7 +36,10 @@ purrr::walk2(.x = folders,
                x <- all_adfs %>% 
                  filter(str_detect(drug_name, .y)) %>% 
                  mutate_if(is.character, str_to_title) %>% 
-                 mutate(strength_uom = str_to_upper(strength_uom))
+                 mutate(ingredients = if_else(drug_name == "Oxycodone Hcl Er", 
+                                              "Oxycodone Hydrochloride", ingredients),
+                        strength_uom = str_to_upper(strength_uom),
+                        ir_er = str_to_upper(ir_er))
                names(x) <- toupper(names(x))
                pattern <- str_replace(.y, "\\W.*", "")
                write_csv(x, str_c(path,glue("{.x}/{pattern}_NDCs.csv")))
